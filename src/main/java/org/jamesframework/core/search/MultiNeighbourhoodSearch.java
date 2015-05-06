b55b98d9ac891dd71d082a8d7fa7c3dcdc70da20
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2015-05-06 16:52:03 +0200

    multi neighbourhood search: make neighbourhood list unmodifiable at construction

--- src/main/java/org/jamesframework/core/search/MultiNeighbourhoodSearch.java
+++ src/main/java/org/jamesframework/core/search/MultiNeighbourhoodSearch.java
@@ -24,15 +24,16 @@ import org.jamesframework.core.problems.Solution;
 import org.jamesframework.core.search.neigh.Neighbourhood;
 
 /**
- * Abstract neighbourhood search that uses <i>multiple</i> neighbourhoods to modify the current solution. This
- * includes variable neighbourhood descent and variable neighbourhood search.
+ * Abstract neighbourhood search that uses <i>multiple</i> neighbourhoods to modify the current solution.
+ * This includes variable neighbourhood descent and variable neighbourhood search.
  *
- * @param <SolutionType> solution type of the problems that may be solved using this search, required to extend {@link Solution}
+ * @param <SolutionType> solution type of the problems that may be solved using this search,
+ *                       required to extend {@link Solution}
  * @author <a href="mailto:herman.debeukelaer@ugent.be">Herman De Beukelaer</a>
  */
 public abstract class MultiNeighbourhoodSearch<SolutionType extends Solution> extends NeighbourhoodSearch<SolutionType> {
 
-    // neighbourhoods
+    // neighbourhoods (unmodifiable view)
     private List<? extends Neighbourhood<? super SolutionType>> neighs;
     
     /**
@@ -40,6 +41,8 @@ public abstract class MultiNeighbourhoodSearch<SolutionType extends Solution> ex
      * modify the current solution. None of both arguments may be <code>null</code> and the list of neighbourhoods
      * may not be empty and may not contain any null elements The default search name "MultiNeighbourhoodSearch"
      * is assigned.
+     * <p>
+     * The search stores an unmodifiable view of the given neighbourhood list.
      * 
      * @throws NullPointerException if <code>problem</code> or <code>neighs</code> are <code>null</code>, or if
      *                              <code>neighs</code> contains a <code>null</code> element
@@ -47,7 +50,8 @@ public abstract class MultiNeighbourhoodSearch<SolutionType extends Solution> ex
      * @param problem problem to be solved
      * @param neighs neighbourhoods used to modify the current solution
      */
-    public MultiNeighbourhoodSearch(Problem<SolutionType> problem, List<? extends Neighbourhood<? super SolutionType>> neighs){
+    public MultiNeighbourhoodSearch(Problem<SolutionType> problem,
+                                    List<? extends Neighbourhood<? super SolutionType>> neighs){
         this(null, problem, neighs);
     }
     
@@ -57,6 +61,8 @@ public abstract class MultiNeighbourhoodSearch<SolutionType extends Solution> ex
      * <code>null</code> and the neighbourhood list may not be empty and may not contain any <code>null</code>
      * elements. The search name may be <code>null</code> in which case the default name "MultiNeighbourhoodSearch"
      * is assigned.
+     * <p>
+     * The search stores an unmodifiable view of the given neighbourhood list.
      * 
      * @throws NullPointerException if <code>problem</code> or <code>neighs</code> are <code>null</code>, or if
      *                              <code>neighs</code> contains a <code>null</code> element
@@ -65,45 +71,51 @@ public abstract class MultiNeighbourhoodSearch<SolutionType extends Solution> ex
      * @param problem problem to be solved
      * @param neighs neighbourhoods used to modify the current solution
      */
-    public MultiNeighbourhoodSearch(String name, Problem<SolutionType> problem, List<? extends Neighbourhood<? super SolutionType>> neighs){
+    public MultiNeighbourhoodSearch(String name, Problem<SolutionType> problem,
+                                    List<? extends Neighbourhood<? super SolutionType>> neighs){
         // pass problem to super
         super(name != null ? name : "MultiNeighbourhoodSearch", problem);
         // check neighs not null
         if(neighs == null){
-            throw new NullPointerException("Error while creating multi neighbourhood search: neighbourhood list can not be null.");
+            throw new NullPointerException("Error while creating multi neighbourhood search: "
+                                            + "neighbourhood list can not be null.");
         }
         // check that neighs does not contain any null elements
         for(Neighbourhood<?> n : neighs){
             if(n == null){
-                throw new NullPointerException("Error while creating multi neighbourhood search: neighbourhood list can not"
-                                                    + " contain any null elements.");
+                throw new NullPointerException("Error while creating multi neighbourhood search: "
+                                                + "neighbourhood list can not contain any null elements.");
             }
         }
         // check neighs not empty
         if(neighs.isEmpty()){
-            throw new IllegalArgumentException("Error while creating multi neighbourhood search: neighbourhood list can not be empty.");
+            throw new IllegalArgumentException("Error while creating multi neighbourhood search: "
+                                                + "neighbourhood list can not be empty.");
         }
         // store neighbourhoods
-        this.neighs = neighs;
+        this.neighs = Collections.unmodifiableList(neighs);
     }
     
     /**
-     * Get an unmodifiable view of the list of applied neighbourhoods.
+     * Get list of applied neighbourhoods (unmodifiable).
      * 
-     * @return unmodifiable view of neighbourhood list
+     * @return neighbourhood list (unmodifiable)
      */
     public List<? extends Neighbourhood<? super SolutionType>> getNeighbourhoods(){
-        return Collections.unmodifiableList(neighs);
+        return neighs;
     }
     
     /**
      * Sets the list of neighbourhoods used to modify the current solution. Note that <code>neighs</code>
      * can not be <code>null</code> nor empty and can not contain any <code>null</code> elements. This method
      * may only be called when the search is idle. It should be used with care for searches that have already
-     * been run before and will be restarted later, as updating the neighbourhoods might break the execution
-     * of a restarted search that tries to continue from where it had arrived.
+     * been run, as updating the neighbourhoods might break the execution of a restarted search that tries to
+     * continue from where it had arrived.
+     * <p>
+     * An unmodifiable view of the given list is stored.
      * 
-     * @throws NullPointerException if <code>neighs</code> is <code>null</code> or contains any <code>null</code> elements
+     * @throws NullPointerException if <code>neighs</code> is <code>null</code>
+     *                              or contains any <code>null</code> elements
      * @throws IllegalArgumentException if <code>neighs</code> is empty
      * @throws SearchException if the search is currently not idle
      * @param neighs list of neighbourhoods used to modify the current solution
@@ -129,7 +141,7 @@ public abstract class MultiNeighbourhoodSearch<SolutionType extends Solution> ex
                 throw new IllegalArgumentException("Can not set neighbourhoods: received empty list.");
             }
             // go ahead
-            this.neighs = neighs;
+            this.neighs = Collections.unmodifiableList(neighs);
         }
     }
     
